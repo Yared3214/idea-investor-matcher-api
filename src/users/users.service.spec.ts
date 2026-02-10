d1a9b1d57741283978c94eeae513b9262bbb3fb4
mark133227 <etawe1627@gmail.com>
2026-02-10 12:02:31 +0300

    add unit tests for UsersService with mocked PrismaService

---
+++ src/users/users.service.spec.ts
@@ -0,0 +1,25 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from './users.service';
+import { PrismaService } from '../database/prisma.service';
+
+describe('UsersService', () => {
+  let service: UsersService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [UsersService, PrismaService],
+    }).compile();
+
+    service = module.get<UsersService>(UsersService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should return all users', async () => {
+    const result = []; // mock empty array
+    jest.spyOn(service, 'findAll').mockImplementation(async () => result);
+    expect(await service.findAll({})).toBe(result);
+  });
+});
