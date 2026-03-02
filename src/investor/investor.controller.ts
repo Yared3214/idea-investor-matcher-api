2f143701292798e9532097561d19183460b43d13
mark133227 <etawe1627@gmail.com>
2026-03-02 13:35:05 +0300

    add view tracking and interest endpoints

--- src/investor/investor.controller.ts
+++ src/investor/investor.controller.ts
@@ -6,35 +6,44 @@ import {
   Body,
   UseGuards,
   Req,
-  Query
+  Query,
+  Delete
 } from '@nestjs/common';
 import { InvestorService } from './investor.service';
 import { InvestorOnboardingDto } from './dto/investor-onboarding.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { GetIdeasQueryDto } from './dto/get-ideas-query.dto';
 
-
-
 @Controller('investor')
+@UseGuards(AuthGuard('jwt'))
 export class InvestorController {
   constructor(private investorService: InvestorService) {}
 
-  @UseGuards(AuthGuard('jwt'))
   @Post('onboarding')
   async onboarding(@Req() req, @Body() dto: InvestorOnboardingDto) {
     return this.investorService.onboarding(req.user.userId, dto);
   }
 
-@UseGuards(AuthGuard('jwt'))
-@Get('ideas')
-async getIdeas(@Req() req, @Query() query: GetIdeasQueryDto) {
-  return this.investorService.getIdeas(req.user.userId,query);
-}
+  @Get('ideas')
+  async getIdeas(@Req() req, @Query() query: GetIdeasQueryDto) {
+    return this.investorService.getIdeas(req.user.userId, query);
+  }
 
-@UseGuards(AuthGuard('jwt'))
-@Get('idea/:id')
-async getSingleIdea(@Param('id') id: string) {
-  return this.investorService.getSingleIdea(id);
-}
+  // 👇 Register view automatically when fetching single idea
+  @Get('idea/:id')
+  async getSingleIdea(@Req() req, @Param('id') id: string) {
+    return this.investorService.getSingleIdea(req.user.userId, id);
+  }
 
+  // 👇 Mark as interested
+  @Post('idea/:id/interested')
+  async markInterested(@Req() req, @Param('id') id: string) {
+    return this.investorService.markInterested(req.user.userId, id);
+  }
+
+  // 👇 Remove interest
+  @Delete('idea/:id/interested')
+  async removeInterest(@Req() req, @Param('id') id: string) {
+    return this.investorService.removeInterest(req.user.userId, id);
+  }
 }
